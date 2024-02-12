308bbeb1185efc98408509932c0519292490663e
Yash Kushwaha <yashkushwaha.x@gmail.com>
2024-02-12 12:05:31 +0530

    project setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- Landing page for student and Admin -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+
+    <section></section>
+
+    <footer></footer>
+
+</body>
+</html>
